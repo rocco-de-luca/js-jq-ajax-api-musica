b57e68118f3051111c88cc45634b03aed70f55a8
rocco-de-luca <roccodelucaboolean@gmail.com>
2020-04-28 20:45:24 +0200

    js-jq-ajax-api-musica

---
+++ js/main.js
@@ -0,0 +1,41 @@
+/**
+ * Attraverso una chiamata ajax all’Api di boolean avremo a 
+ * disposizione una decina di dischi musicali. 
+Servendoci di handlebars stampiamo tutto a schermo.
+In questo momento non è importante la parte grafica.
+
+*/
+
+$(document).ready(function () {
+
+    // Refs
+    var apiMusic = 'https://flynn.boolean.careers/exercises/api/array/music';
+    var cd = $('.cd');
+
+    // Init Handlebars
+    var source = $('#album-template').html();
+    var template = Handlebars.compile(source);
+    
+    $.ajax({
+        url: apiMusic,
+        method: 'GET',
+        success: function (data) {
+            var albums = data.response;
+            for (var i = 0; i < albums.length; i++) {
+              var item = template(albums[i]);
+                console.log(item);
+               cd.append(item);
+            }
+           
+        },
+        error: function () {
+            console.log('Si è verificato un errore');
+        }
+    });
+
+});
+
+/**
+ * Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz.
+In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+*/
